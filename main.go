4bd4762631b1e5684d68b4a55b301b8816eac224
aswinnarayanan <aswinn89@gmail.com>
2023-04-15 09:02:34 +0000

    Read file bytes into buffer and loop through

--- main.go
+++ main.go
@@ -1,24 +1,61 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"io/fs"
 	"log"
+	"os"
 	"path/filepath"
 )
 
-func fileInfo(path string, file fs.DirEntry, err error) error {
+func uncacheFile(path string, err error) error {
 	if err != nil {
 		return err
 	}
-	if !file.IsDir() {
-		fmt.Println(path)
+	// Read byte 67108864
+	f, err := os.Open(path)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+	r := bufio.NewReader(f)
+	buf := make([]byte, 67108864)
+	for {
+		n, err := io.ReadFull(r, buf[:cap(buf)])
+		buf = buf[:n]
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println(path, "| UNCACHED")
+				break
+			}
+			if err != io.ErrUnexpectedEOF {
+				fmt.Fprintln(os.Stderr, err)
+				break
+			}
+		}
+		fmt.Println(path, "|", n)
+	}
+	if err != nil {
+		return err
 	}
 	return nil
 }
 
 func main() {
-	err := filepath.WalkDir(".", fileInfo)
+	inputDir := "./tmp"
+	err := filepath.WalkDir(inputDir,
+		func(path string, file fs.DirEntry, err error) error {
+			if err != nil {
+				return err
+			}
+			if !file.IsDir() {
+				uncacheFile(path, err)
+			}
+			return nil
+		})
+	fmt.Println("== COMPLETED ==")
 	if err != nil {
 		log.Println(err)
 	}
