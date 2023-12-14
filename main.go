77cd678ed759c47a042f50198007b1f544a7ff6f
aswinnarayanan <aswin@excelsol.com>
2023-12-14 06:12:20 +0000

    Switch log to fmt Prevents msgs going to stderr

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io"
 	"io/fs"
-	"log"
 	"os"
 	"path/filepath"
 	"sync"
@@ -18,10 +17,10 @@ func main() {
 		timePoint := timeStart
 		totalCount := 0
 		for _, inputDir := range os.Args[1:] {
-			log.Println("Recalling", inputDir)
+			fmt.Println("Recalling", inputDir)
 
 			if _, err := os.Stat(inputDir); os.IsNotExist(err) {
-				log.Println("Input directory does not exist")
+				fmt.Println("Input directory does not exist")
 			} else {
 				wg := sync.WaitGroup{}
 				wc := make(chan struct{}, 100)
@@ -31,7 +30,7 @@ func main() {
 					func(filePath string, file fs.DirEntry, err error) error {
 						if !file.IsDir() {
 							count++
-							fmt.Printf(".")
+							// fmt.Printf(".")
 
 							wg.Add(1)
 							go func(count int) {
@@ -45,19 +44,19 @@ func main() {
 					})
 				wg.Wait()
 				if err != nil {
-					log.Println(err)
+					fmt.Println(err)
 				}
 				fmt.Printf("")
-				log.Println("Recalled", count, "files in", time.Since(timePoint))
+				fmt.Println("Recalled", count, "files in", time.Since(timePoint))
 				timePoint = time.Now()
 				totalCount += count
 			}
 			fmt.Println()
 		}
 		fmt.Println("===================")
-		log.Println("Recalled", totalCount, "files in", time.Since(timeStart))
+		fmt.Println("Recalled", totalCount, "files in", time.Since(timeStart))
 	} else {
-		log.Println("Please provide the input directory")
+		fmt.Println("Please provide the input directory")
 	}
 }
 
@@ -66,6 +65,7 @@ func uncacheFile(filePath string, count int) error {
 
 	fileHandle, _ := os.Open(filePath)
 	fileReader := bufio.NewReader(fileHandle)
+	fmt.Println(filePath)
 	for {
 		_, err := fileReader.Read(dataBuffer)
 		if err != nil {
