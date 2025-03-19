ca994fd81be8d2354f5eb9425c971f453697b9c7
aswinnarayanan <aswin@excelsol.com>
2025-03-19 01:05:25 +0000

    Add logging, improvements to error and std outputs

--- main.go
+++ main.go
@@ -5,76 +5,100 @@ import (
 	"fmt"
 	"io"
 	"io/fs"
+	"log"
 	"os"
 	"path/filepath"
+	"strings"
 	"sync"
 	"time"
 )
 
 func main() {
-	if os.Args != nil && len(os.Args) > 1 {
-		timeStart := time.Now()
-		timePoint := timeStart
-		totalCount := 0
-		for _, inputDir := range os.Args[1:] {
-			fmt.Println("\n> Recalling from", inputDir)
-
-			if _, err := os.Stat(inputDir); os.IsNotExist(err) {
-				fmt.Println("Input directory does not exist")
-			} else {
-				wg := sync.WaitGroup{}
-				wc := make(chan struct{}, 100)
-				count := 0
-
-				err := filepath.WalkDir(inputDir,
-					func(filePath string, file fs.DirEntry, err error) error {
-						if !file.IsDir() {
-							count++
-
-							wg.Add(1)
-							go func(count int) {
-								wc <- struct{}{}
-								uncacheFile(filePath)
-								<-wc
-								wg.Done()
-							}(count)
-						}
-						return nil
-					})
-				wg.Wait()
-				if err != nil {
-					fmt.Println(err)
-				}
-				fmt.Println("<", count, "files in", time.Since(timePoint))
-				timePoint = time.Now()
-				totalCount += count
-			}
-		}
-		fmt.Println("\n===================")
-		fmt.Println("Recalled", totalCount, "files in", time.Since(timeStart))
-		fmt.Println("===================")
-	} else {
-		fmt.Println("Please provide the input directory")
+	if len(os.Args) <= 1 {
+		log.Println("Please provide the input directory")
+		return
 	}
+
+	timeStart := time.Now()
+	totalCount := 0
+
+	for _, inputDir := range os.Args[1:] {
+		log.Println("> Recalling from", inputDir)
+
+		if _, err := os.Stat(inputDir); os.IsNotExist(err) {
+			log.Println("Input directory does not exist:", inputDir)
+			continue
+		}
+
+		count, err := recallFiles(inputDir)
+		if err != nil {
+			log.Println("Error recalling files:", err)
+			continue
+		}
+
+		totalCount += count
+	}
+
+	resultText := fmt.Sprintf("Recalled %d files in %s", totalCount, time.Since(timeStart))
+	separator := strings.Repeat("=", len(resultText))
+
+	log.Println(separator)
+	log.Println(resultText)
+	log.Println(separator)
 }
 
+// recallFiles processes all files in the given directory
+func recallFiles(inputDir string) (int, error) {
+	wg := sync.WaitGroup{}
+	wc := make(chan struct{}, 100)
+	count := 0
+
+	err := filepath.WalkDir(inputDir, func(filePath string, file fs.DirEntry, err error) error {
+		if err != nil {
+			return fmt.Errorf("error accessing file %s: %w", filePath, err)
+		}
+		if !file.IsDir() {
+			count++
+			wg.Add(1)
+			go func(filePath string) {
+				defer wg.Done()
+				wc <- struct{}{}
+				if err := uncacheFile(filePath); err != nil {
+					log.Println("Error uncaching file:", err)
+				}
+				<-wc
+			}(filePath)
+		}
+		return nil
+	})
+	wg.Wait()
+	if err != nil {
+		return 0, fmt.Errorf("error walking directory %s: %w", inputDir, err)
+	}
+
+	log.Println("<", count, "files in", time.Since(time.Now()))
+	return count, nil
+}
+
+// uncacheFile reads the file to uncache it
 func uncacheFile(filePath string) error {
 	dataBuffer := make([]byte, 4096)
 
-	fileHandle, _ := os.Open(filePath)
+	fileHandle, err := os.Open(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to open file %s: %w", filePath, err)
+	}
+	defer fileHandle.Close()
+
 	fileReader := bufio.NewReader(fileHandle)
-	// fmt.Printf(".")
 	for {
 		_, err := fileReader.Read(dataBuffer)
 		if err != nil {
 			if err == io.EOF {
 				break
-			} else {
-				fmt.Println(filePath, ":", err)
-				return err
 			}
+			return fmt.Errorf("failed to read file %s: %w", filePath, err)
 		}
 	}
-	fileHandle.Close()
 	return nil
 }
